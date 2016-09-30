512fba751b9c45a35321829846ac206d7e097771
Krista Calderon <kamsci@gmail.com>
2016-09-29 17:29:54 -0700

    added template form to new-message, created new-message.template.html

--- assets/app/messages/new-message.component.ts
+++ assets/app/messages/new-message.component.ts
@@ -4,21 +4,15 @@ import { MessageService } from './message.service';
 
 @Component({
     selector: 'my-new-message',
-    template: `
-        <section class="col-sm-10 col-sm-offset-1">
-            <div class="form-group">
-                <input type="text" class="form-control" id="content" placeholder="Type your message.." #input>
-            </div>
-            <button type="submit" class="btn btn-primary" (click)="onCreate(input.value)">Send</button>
-        </section>`,
+    templateUrl: './js/app/messages/new-message.template.html',
     styleUrls: ['./js/app/app.style.css']
 })
 export class NewMessageComponent {
     // Inject an instance of MessagesService (dependency) on build of this compenent, call it _messageServices
     constructor(private _messageService: MessageService) {};
 
-    onCreate(content: string) {
-        const message: Message = new Message(content, null, 'Platapus');
+    onSubmit(form:any) {
+        const message: Message = new Message(form.content, null, 'Platapus');
         this._messageService.addMessage(message);
     }
     
